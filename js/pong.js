cd16f8801f07501314351531c6c094f5c6d94f45
david <d.marsalone@gmail.com>
2021-01-04 10:05:05 +0100

    Simplification du code

--- js/pong.js
+++ js/pong.js
@@ -12,18 +12,14 @@ let balle=new Balle($(".balle"));
  * Le joueur à gauche
  * @type {Joueur}
  */
-let joueur1=new Joueur(
-    $(".joueur1")
-);
+let joueur1=new Joueur($(".joueur1"));
 
 /**
  * Le joueur à droite
  * Ici, pour que ce soit plus lisible, on saute des lignes mais c'est pareil que pour joueur 1
  * @type {Joueur}
  */
-let joueur2=new Joueur(
-    $(".joueur2")
-);
+let joueur2=new Joueur($(".joueur2"));
 
 /**
  * Gère les inerractions utilisateurs (les mamifères qui jouent au jeu), clavier, tactile...
